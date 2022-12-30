82f5835c79a5b2cfd3665de2fa6e9d4d90e44e12
mahfuz-raihan <mahfuz_raihan@outlook.com>
2022-12-30 09:45:14 +0600

    FastAPI

--- video_stream/app.py
+++ video_stream/app.py
@@ -4,11 +4,47 @@ import cv2 as cv
 # initialize the Flask app
 app = Flask(__name__)
 
-cap = cv.VideoCapture('/home/mahfuz/Downloads/momo.mp4')
 
-def gen_frames():
+def gen_frames1():
+    cap1 = cv.VideoCapture('/home/mahfuz/Desktop/video/cricket.mp4')
     while True:
-        success, frame = cap.read()
+        success, frame = cap1.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+        yield(b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+def gen_frames2():
+    cap2 = cv.VideoCapture('/home/mahfuz/Desktop/video/basketball.mp4')
+    while True:
+        success, frame = cap2.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+        yield(b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+def gen_frames3():
+    cap3 = cv.VideoCapture('/home/mahfuz/Desktop/video/boxing.mp4')
+    while True:
+        success, frame = cap3.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+        yield(b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+def gen_frames4():
+    cap4 = cv.VideoCapture('/home/mahfuz/Desktop/video/football.mp4')
+    while True:
+        success, frame = cap4.read()
         if not success:
             break
         else:
@@ -18,13 +54,27 @@ def gen_frames():
                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen_frames1(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video_feed2')
+def video_feed2():
+    return Response(gen_frames2(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video_feed3')
+def video_feed3():
+    return Response(gen_frames3(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video_feed4')
+def video_feed4():
+    return Response(gen_frames4(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
